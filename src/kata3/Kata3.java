678a80670da7337e0addb0a19ed6c1fcab602e92
usuario <usuario@10.22.148.64>
2017-10-20 12:04:03 +0100

    Kata3 - versión 4

--- src/kata3/Kata3.java
+++ src/kata3/Kata3.java
@@ -5,19 +5,10 @@ public class Kata3 {
 
     public static void main(String[] args) {
         Histogram histogram = new Histogram();
-        histogram.increment(1);
-        histogram.increment(1);
-        histogram.increment(1);
-        histogram.increment(1);
-        histogram.increment("gmail.com");
-        histogram.increment("ulpgc.es");
-        histogram.increment("ulpgc.es");
-        histogram.increment("ulpgc.es");
-        histogram.increment("ulpgc.es");
-        histogram.increment("ulpgc.es");
-        histogram.increment("ull.es");
-        histogram.increment("ull.es");
-        histogram.increment("hotmail.com");
+        histogram.increment(1,3);
+        histogram.increment("gmail.com",4);
+        histogram.increment("ulpgc.es",2);
+        histogram.increment("ull.es",5);
         new HistogramDisplay(histogram).execute();
     }
  
