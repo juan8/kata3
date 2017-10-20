0997aead045c9396643277970d5158bf285dee87
usuario <usuario@10.22.148.64>
2017-10-20 11:40:35 +0100

    Kata3 - versión 2

--- src/kata3/Kata3.java
+++ src/kata3/Kata3.java
@@ -4,7 +4,18 @@ package kata3;
 public class Kata3 {
 
     public static void main(String[] args) {
-        HistogramDisplay histo = new HistogramDisplay();
-        histo.execute();
+        Histogram histogram = new Histogram();
+        histogram.increment("gmail.com");
+        histogram.increment("gmail.com");
+        histogram.increment("gmail.com");
+        histogram.increment("ulpgc.es");
+        histogram.increment("ulpgc.es");
+        histogram.increment("ulpgc.es");
+        histogram.increment("ulpgc.es");
+        histogram.increment("ulpgc.es");
+        histogram.increment("ull.es");
+        histogram.increment("ull.es");
+        histogram.increment("hotmail.com");
+        new HistogramDisplay(histogram).execute();
     }
 }
